11a61c90b4daaad436fc0176cf0045b0b07f608d
roshinys <roshinchaya@gmail.com>
2023-08-09 13:19:50 +0530

    fixed add-todo api

--- pages/api/add-todo.js
+++ pages/api/add-todo.js
@@ -1,25 +1,25 @@
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
-  const client = await MongoClient.connect(process.env.MONGO_URI);
+  let client = null;
   try {
+    client = await MongoClient.connect(process.env.MONGO_URI);
     if (req.method === "POST") {
       const data = req.body;
       const db = client.db();
       const todosCollection = db.collection("todos");
       const result = await todosCollection.insertOne(data);
-      client.close();
       res
         .status(201)
         .json({ success: true, message: "Todos inserted!", todoItem: result });
     }
   } catch (err) {
     console.log(err);
-    return res
-      .status(500)
-      .json({ success: false, message: "Failed to add todo" });
+    res.status(500).json({ success: false, message: "Failed to add todo" });
   } finally {
-    client.close();
+    if (client) {
+      client.close();
+    }
   }
 }
 
