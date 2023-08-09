14015ebd6b8a446ef2601a9e78c7335c9d304038
roshinys <roshinchaya@gmail.com>
2023-08-09 09:49:01 +0530

    styled header

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./Header.module.css";
-import Image from "next/image";
 import Avatar from "react-avatar";
 
 function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerLayout}>
+        <h1>Todo App</h1>
+        <div className={styles.rightHeaderLayout}>
+          <h3>Roshin</h3>
+          <Avatar name="Roshin" round color="#0055D1" size="50" />
+        </div>
+      </div>
+      {/* <div className={styles.headerLayout}>
         <Image
           src="https://links.papareact.com/c2cdd5"
           alt="logo"
@@ -23,7 +29,7 @@ function Header() {
           </form>
           <Avatar name="Roshin" round color="#0055D1" size="50" />
         </div>
-      </div>
+      </div> */}
     </header>
   );
 }
